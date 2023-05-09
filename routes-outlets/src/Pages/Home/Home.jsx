2295f9b83cf87accc5e3dcc9b1b1ea2125d55b2d
Emma Nielsen <Emmanielsen843@gmail.com>
2023-05-09 10:45:07 +0200

    installed sass and styled my form

--- routes-outlets/src/Pages/Home/Home.jsx
+++ routes-outlets/src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Home.scss";
 
 const Home = () => {
   // Bruges til og reset vores inputs med
@@ -7,7 +8,7 @@ const Home = () => {
     lastname: "",
     address: "",
     city: "",
-    zipcode: ""
+    zipcode: "",
   };
 
   const [state, setState] = useState(intialState);
@@ -15,7 +16,7 @@ const Home = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
 
-    setState(prev => ({...prev, [name]: value }))
+    setState((prev) => ({ ...prev, [name]: value }));
   };
 
   // Bruges så hvis ikke behøver at skrive state.name osv nede i vores value.
@@ -24,19 +25,54 @@ const Home = () => {
   console.log(state);
 
   return (
-    <form>
-      <input type="text" name="name" value={name} onChange={handleChange} placeholder="Fornavn" />
-      <input type="text" name="lastname" value={lastname} onChange={handleChange} placeholder="Efternavn" />
-      <input type="text" name="address" value={address} onChange={handleChange} placeholder="Adresse" />
-      <input type="text" name="city" value={city} onChange={handleChange} placeholder="By" />
-      <input type="text" name="zipcode" value={zipcode} onChange={handleChange} placeholder="Zipcode" />
+    <form className="form-main">
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        placeholder="Fornavn"
+      />
+      <input
+        type="text"
+        name="lastname"
+        value={lastname}
+        onChange={handleChange}
+        placeholder="Efternavn"
+      />
+      <input
+        type="text"
+        name="address"
+        value={address}
+        onChange={handleChange}
+        placeholder="Adresse"
+      />
+      <input
+        type="text"
+        name="city"
+        value={city}
+        onChange={handleChange}
+        placeholder="By"
+      />
+      <input
+        type="text"
+        name="zipcode"
+        value={zipcode}
+        onChange={handleChange}
+        placeholder="Zipcode"
+      />
 
-      <br />
-      Firstname: {name} <br />
-      Lastname: {lastname} <br />
-      Address: {address} <br />
-      City: {city} <br />
-      Zipcode: {zipcode} <br />
+      <div className="formInfo">
+        <div className="names">
+          Firstname: {name} <br />
+          Lastname: {lastname}
+        </div>
+        <div className="cityInfo">
+          Address: {address} <br />
+          City: {city} <br />
+          Zipcode: {zipcode}
+        </div>
+      </div>
 
       <button onClick={() => setState(intialState)}>Reset</button>
     </form>
