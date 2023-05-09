36e0d6bbcdd20ddc66c7b027369f7a7043ef4082
Emma Nielsen <Emmanielsen843@gmail.com>
2023-05-09 09:48:58 +0200

    form

--- routes-outlets/src/Pages/Home/Home.jsx
+++ routes-outlets/src/Pages/Home/Home.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Home = () => {
-  return (
-    <>
-    <h1>HOME PAGE</h1>
-    </>
-  )
-}
+  // Bruges til og reset vores inputs med
+  const intialState = {
+    name: "",
+    lastname: "",
+    address: "",
+    city: "",
+  };
 
-export default Home
+  const [state, setState] = useState(intialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setState(prev => ({...prev, [name]: value }))
+  };
+
+  // Bruges så hvis ikke behøver at skrive state.name osv nede i vores value.
+  const { name, lastname, address, city } = state;
+
+  console.log(state);
+
+  return (
+    <form>
+      <input type="text" name="name" value={name} onChange={handleChange} placeholder="Fornavn" />
+      <input type="text" name="lastname" value={lastname} onChange={handleChange} placeholder="Efternavn" />
+      <input type="text" name="address" value={address} onChange={handleChange} placeholder="Adresse" />
+      <input type="text" name="city" value={city} onChange={handleChange} placeholder="By" />
+      <input type="text" name="zipcode" value={state.zipcode} onChange={handleChange} placeholder="Zipcode" />
+
+      <br />
+      Firstname: {name} <br />
+      Lastname: {lastname} <br />
+      Address: {address} <br />
+      City: {city} <br />
+      Zipcode: {state.zipcode} <br />
+
+      <button onClick={() => setState(intialState)}>Reset</button>
+    </form>
+  );
+};
+
+export default Home;
